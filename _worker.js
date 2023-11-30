e44c96cd0a7bba80a45dc2ba2fe7713c7e427c44
xletu <136406305+Ailetu@users.noreply.github.com>
2023-11-30 19:59:21 +0800

    Update worker.js
    简单反代网址

--- _worker.js
+++ _worker.js
@@ -1,23 +1,7 @@
 export default {
-    async fetch(request, env, ctx) {
-        const url = new URL(request.url);
-        let proxyUrl = url.pathname.replace('/', '');
-
-        try {
-            proxyUrl = new URL(proxyUrl)
-        } catch(error) {
-            console.log('error is', error)
-            return new Response('Bad request: Missing proxy url', { status: 404 })
-        }
-
-        const req = new Request(proxyUrl, {
-            redirect: 'follow'
-        });
-
-        let res = await fetch(req);
-        res = new Response(res.body, res);
-        res.headers.set('Access-Control-Allow-Origin', '*');
-
-        return res;
-    },
-}; 
+  async fetch(request, env, ctx) {
+    const url = new URL(request.url);
+    url.hostname = 'api.openai.com';
+    return await fetch(new Request(url, request));
+  },
+};
